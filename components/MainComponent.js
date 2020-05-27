2ea13b53e3454bb00471925cf8ab0dc905d2a189
Stephanie Raymos <stephanieraymos@gmail.com>
2020-05-27 13:29:49 -0700

    Created Main Component

---
+++ components/MainComponent.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Directory from './DirectoryComponent';
+import { ADMINS } from '../shared/admins';
+import AdminInfo from './AdminInfoComponent';
+import { View } from 'react-native';
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            admins: admins,
+            selectedAdmin: null
+        };
+    }
+
+    onAdminSelect(adminId) {
+        this.setState({selectedAdmin: adminId});
+    }
+
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <Directory 
+                    admin={this.state.admins} 
+                    onPress={adminId => this.onAdminSelect(adminId)} />
+                <AdminInfo 
+                    admin={this.state.admins.filter(admin => admin.id === this.state.selectedAdmin)[0]}
+                />
+            </View>
+        );
+    }
+}
+
+export default Main;
