71e08096be40fa32326106be58dee5548189400f
Stephanie Raymos <stephanieraymos@gmail.com>
2020-05-27 14:41:28 -0700

    Created promotions

---
+++ shared/promotions.js
@@ -0,0 +1,9 @@
+export const PROMOTIONS = [
+  {
+      id: 0,
+      name: "Home Chef",
+      image: "",
+      featured: true,
+      description: "Get $20.00 off your first Home Chef delivery!"
+  }
+]
