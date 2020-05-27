44413d98a6d9ee86bdeefb6f0b4fe4ff0e2a7caf
Stephanie Raymos <stephanieraymos@gmail.com>
2020-05-27 13:22:56 -0700

    Created admins array

---
+++ shared/admins.js
@@ -0,0 +1,34 @@
+export const ADMINS = [
+  {
+      id: 0,
+      name: "Stephanie Raymos",
+      image: "/components/images/stephanie.jpg",
+      status: Creator,
+      featured: true,
+      description: "Created the Deals and Codes Community facebook group in 2018 | Created the Deals and Codes Community website in 2019 | Created the Deals and Codes Community app in 2020"
+  },
+  {
+      id: 1,
+      name:"Audie Raymos",
+      image: "/components/images/audie.jpg",
+      status: "admin",
+      featured: true,
+      description: "Came onto Deals and Codes Community as a valued admin in 2019"
+  },
+  {
+      id: 2,
+      name:"Laura Nelson",
+      image: "/components/images/laura.jpg",
+      status: "admin",
+      featured: false,
+      description: "Came onto Deals and Codes Community as a valued admin in 2019"
+  },
+  {
+      id: 3,
+      name: "Skyler Nordquist",
+      image: "/components/images/skyler.jpg",
+      status: "admin",
+      featured: false,
+      description: "Came onto Deals and Codes Community as a valued admin in 2019"
+  }
+];
