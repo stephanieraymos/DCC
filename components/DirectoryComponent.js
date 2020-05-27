d647997b2e391f5441d3116ee39e202d680f971b
Stephanie Raymos <stephanieraymos@gmail.com>
2020-05-27 13:55:35 -0700

    Updated Directory

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import { ADMINS } from '../shared/admins';
 
-function Directory(props) {
 
-    const renderDirectoryItem = ({item}) => {
-        return (
-            <ListItem
-                title={item.name}
-                subtitle={item.description}
-                onPress={() => props.onPress(item.id)}
-                leftAvatar={{ source: require('./images/stephanie.jpg')}}
-            />
-        );
+class Directory extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      admins: ADMINS
+    };
+  }
+
+  static navigationOptions = {
+    title: 'Directory'
+  };
+
+  render() {
+    const { navigate } = this.props.navigation;
+    const renderDirectoryItem = ({ item }) => {
+      return (
+        <ListItem
+          title={item.name}
+          subtitle={item.description}
+          onPress={() => navigate('AdminInfo', { adminId: item.id })}
+          leftAvatar={{ source: require('./images/stephanie.jpg') }}
+        />
+      );
     };
 
     return (
-        <FlatList 
-            data={props.admins}
-            renderItem={renderDirectoryItem}
-            keyExtractor={item => item.id.toString()}
-        />
+      <FlatList
+        data={this.state.admins}
+        renderItem={renderDirectoryItem}
+        keyExtractor={item => item.id.toString()}
+      />
     );
-}
+  }
 
+}
 export default Directory;
