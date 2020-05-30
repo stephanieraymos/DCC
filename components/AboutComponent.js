fc07a77f0e975c6a7139c6080674b75e48f34aa2
Stephanie Raymos <stephanieraymos@gmail.com>
2020-05-30 14:35:38 -0700

    Updated the mission statement in About

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,16 +1,25 @@
-import React, { Component } from 'react'
-import { Text, ScrollView, FlatList } from 'react-native';
+import React, { Component } from 'react';
+import { Text, ScrollView, FlatList, Linking, TouchableOpacity, View } from 'react-native';
 import { PARTNERS } from '../shared/partners';
 import { Card, ListItem } from 'react-native-elements';
 
-function Mission () {
+function Mission() {
 
   return (
     <Card
-    title={"Our Mission"}>
-    <Text style={{margin: 10}}>
-    We are dedicated to providing amazing deals to help save you tons of money.
-    </Text>
+      title={"Our Mission"}>
+      <View style={{ display: "flex", flexDirection: "row", flex: 1, flexWrap: 'wrap', margin: 10 }}>
+        <Text>The Deals and Codes Community </Text>
+        <TouchableOpacity>
+          <Text style={{ color: 'blue' }} onpress={() => Linking.openURL('https://www.facebook.com/groups/dealsandcodescommunity/')} >
+          facebook group
+          </Text>
+        </TouchableOpacity>
+        <Text>was created in 2018 by Stephanie Raymos. 
+          This community is a place to find amazing deals for many retailers such as Amazon and Walmart.  
+          This app was created nearly two years later to create a more user-friendly browsing experience for everyone. 
+        </Text>
+      </View>
     </Card>
   )
 }
@@ -28,8 +37,8 @@ class AboutComponent extends Component {
     title: 'About Us'
   }
   render() {
-  
-    
+
+
     const renderPartner = ({ item }) => {
       return (
         <ListItem
@@ -48,8 +57,8 @@ class AboutComponent extends Component {
           <FlatList
             data={this.state.partners}
             renderItem={renderPartner}
-            keyExtractor={item => item.id.toString()} 
-            />
+            keyExtractor={item => item.id.toString()}
+          />
         </Card>
       </ScrollView>
     );
